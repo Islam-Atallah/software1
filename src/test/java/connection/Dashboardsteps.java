5f8ec53c39335213f3976bcf9117d9cb297ef54a
islam atallah <atallahislam006@gmail.com>
2023-07-09 00:54:23 +0300

    testt

---
+++ src/test/java/connection/Dashboardsteps.java
@@ -0,0 +1,45 @@
+package connection;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Dashboardsteps {
+    @Given("a residence named {string} with {int} floors")
+    public void a_residence_named_with_floors(String string, Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("the residence has tenants")
+    public void the_residence_has_tenants() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the dashboard owner selects {string}")
+    public void the_dashboard_owner_selects(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the number of tenants should be displayed")
+    public void the_number_of_tenants_should_be_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the number of floors should be displayed")
+    public void the_number_of_floors_should_be_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the apartments on a specific floor should be displayed")
+    public void the_apartments_on_a_specific_floor_should_be_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the apartment details should be displayed")
+    public void the_apartment_details_should_be_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
