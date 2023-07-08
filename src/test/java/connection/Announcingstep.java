2e0f1ca88a087ad63e3a409db0b653bbc66b56df
islam atallah <atallahislam006@gmail.com>
2023-07-09 00:51:53 +0300

    testt

---
+++ src/test/java/connection/Announcingstep.java
@@ -0,0 +1,62 @@
+package connection;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Announcingstep {
+    @Given("I have a private residence named {string}")
+    public void i_have_a_private_residence_named(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I add the following photos:")
+    public void i_add_the_following_photos(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I set the residence location to {string}")
+    public void i_set_the_residence_location_to(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I set the residence information to {string}")
+    public void i_set_the_residence_information_to(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I set the available services to:")
+    public void i_set_the_available_services_to(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I set the monthly rent for the residence to ${int} inclusive of utilities")
+    public void i_set_the_monthly_rent_for_the_residence_to_$_inclusive_of_utilities(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I set the contact information to {string}")
+    public void i_set_the_contact_information_to(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the residence should be announced successfully")
+    public void the_residence_should_be_announced_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
