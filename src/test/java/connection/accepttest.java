8bc6c55f697b1f2b74a46a5b80775750b1259452
islam atallah <atallahislam006@gmail.com>
2023-07-09 00:57:56 +0300

    testt

---
+++ src/test/java/connection/accepttest.java
@@ -0,0 +1,22 @@
+package connection;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+
+
+
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "features",
+        plugin = {"summary","html:target/cucumber/wikipedia.html"},
+        monochrome = true,
+        snippets = SnippetType.CAMELCASE,
+glue= "connection"
+)
+public class accepttest {
+}
